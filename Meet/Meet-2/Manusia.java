980d6888cf37d7f2cdd7dd5f46d11786bde41d89
Naufal Ridho Permana <naufalriper03@gmail.com>
2026-02-26 14:32:39 +0700

    edit file Manusia dan Tes

--- Meet/Meet-2/Manusia.java
+++ Meet/Meet-2/Manusia.java
@@ -1,1 +1,20 @@
+class Manusia{
+    //properties
 
+    String nama;
+
+
+
+    Manusia(){
+        nama = "Nama aku Michelle";
+    }
+    void getNama(){
+        System.out.println(nama);
+    }
+    public static void main(String[] args) {
+        Manusia michelle = new Manusia();
+        System.out.println(michelle.nama);
+        michelle.nama = "Michelle";
+        System.out.println(michelle.nama);
+    }
+}
