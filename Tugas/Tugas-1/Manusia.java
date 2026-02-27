22ad71799fc66e7384c935b54dab7169489cf0a6
Naufal Ridho Permana <naufalriper03@gmail.com>
2026-02-27 17:32:30 +0700

    nambah fungsi

--- Tugas/Tugas-1/Manusia.java
+++ Tugas/Tugas-1/Manusia.java
@@ -1,10 +1,21 @@
 class Manusia  {
     String nama;
-    boolean punyaTv;
+    boolean punyaTv = false;
 
     //konstruktor kosng
     public Manusia(){
 
     }
+
+    //Method
+    public Manusia (String nama){
+        this.nama = nama;
+    }
+
+    public String namaSaya(){
+        return nama;
+    }
+
     
+
 }
