ba5d16f72641299a4192a0f499bc482faa6ebb75
Naufal Ridho Permana <naufalriper03@gmail.com>
2026-02-27 15:56:24 +0700

    bikin buat nyalain dan matiinnya

--- Tugas/Tugas-1/TV.java
+++ Tugas/Tugas-1/TV.java
@@ -1,6 +1,23 @@
 class TV {
+
+    //atributnya
     int channel = 1;
     int VolumeLevel = 1;
     boolean on = false;    
+
+    //contructornya
+    public TV() {
+
+    }
+
+    //nyalain tv
+    public void turnOn() {
+        on = true;
+    }
+
+    public void turnOff(){
+        on = false;
+    }
+
 }
 
