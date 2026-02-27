74d3ca8b53f7116f1ec3acbc1cc4bfa1ef92fa54
Naufal Ridho Permana <naufalriper03@gmail.com>
2026-02-27 16:02:37 +0700

    nambahin fungsi yang lain, tapi blm ada isinya

--- Tugas/Tugas-1/TV.java
+++ Tugas/Tugas-1/TV.java
@@ -2,7 +2,7 @@ class TV {
 
     //atributnya
     int channel = 1;
-    int VolumeLevel = 1;
+    int volumeLevel = 1;
     boolean on = false;    
 
     //contructornya
@@ -15,9 +15,41 @@ class TV {
         on = true;
     }
 
+    //matiin tv
     public void turnOff(){
         on = false;
     }
 
+    //set channel
+    public void setChannel (new channel){
+
+
+    }
+
+    //set volume
+    public void setVolume(new volumeLevel){
+
+    }
+
+    //naikin channel
+    public void channelUp(){
+
+    }
+
+    //turun channel
+    public void channelDown(){
+
+    }
+
+    //naikin volume
+    public volumeUp(){
+
+    }
+
+    //turunin volume
+    public volumeDown(){
+
+    }
+    
 }
 
