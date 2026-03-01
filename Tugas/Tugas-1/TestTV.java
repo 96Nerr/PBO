cf4b0de4ba0350376c7a5ad47b718ba4afa776e0
Naufal Ridho Permana <naufalriper03@gmail.com>
2026-03-01 23:30:28 +0700

    final (kelar)

--- Tugas/Tugas-1/TestTV.java
+++ Tugas/Tugas-1/TestTV.java
@@ -7,14 +7,14 @@ public class TestTV{
 
         TV tv2 = new TV();
         tv2.turnOn();
-        tv2.setChannel(120);
-        tv2.setVolume(2);
+        tv2.channelUp();
+        tv2.channelUp();
+        tv2.volumeUp();
 
-        System.out.println("tv1`s channel is " + tv1.channel);
-        System.out.println("tv1`s volume is " + tv1.volumeLevel);
-        System.out.println("tv2`s channel is " + tv2.channel);
-        System.out.println("tv2`s volume is " + tv2.volumeLevel + "\n");
-
+        System.out.println("tv1`s channel is " + tv1.channel
+        + " and volume level is " + tv1.volumeLevel);
+        System.out.println("tv2`s channel is " + tv2.channel
+        + " and volume level is " + tv2.volumeLevel);
 
         Manusia man1 = new Manusia("Erik");
         System.out.println(man1.namaSaya());
