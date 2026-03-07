c6418d73f5af9e27e37213b0ed67d381c3e1c229
“96Nerr” <naufalriper03@gmail.com>
2026-03-07 19:46:39 +0700

    membuat 3 konstruktor

---
+++ Tugas/Tugas-2/GamePlayer.java
@@ -0,0 +1,18 @@
+public class GamePlayer {
+    
+    double height;
+    double weight;
+
+    public GamePlayer(){
+
+    }
+
+    public GamePlayer(double height, double weight){
+        
+    }
+
+    public GamePlayer(double height, double weight, int positionX, int positionY){
+        
+    }
+
+}
