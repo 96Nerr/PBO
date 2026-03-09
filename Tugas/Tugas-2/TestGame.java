9d1eb6ac3d448cadb48f66ae55cf2a1c7bfd1621
Naufal Ridho Permana <naufalriper03@gmail.com>
2026-03-10 01:06:57 +0700

    ngisi file test game, buat diuji

--- Tugas/Tugas-2/TestGame.java
+++ Tugas/Tugas-2/TestGame.java
@@ -2,7 +2,18 @@ public class TestGame{
     public static void main(String [] args){
 
         GamePlayer Player1 = new GamePlayer();
+        Player1.setDimension(150, 85);
+        Player1.setPosition(10, 7);
+
         GameEnemy Enemy1 = new GameEnemy();
+        Enemy1.setDimension(150, 85);
+        Enemy1.setPosition(5, 5);
+
+        Player1.Run(10);
+        System.out.println("Player1 position X = " + Player1.getX() + " position Y = " + Player1.getY());
+
+        Enemy1.Run();
+        System.out.println("Enemy1 position X = " + Enemy1.getX() + " position Y = " + Enemy1.getY());
 
     }
 }
