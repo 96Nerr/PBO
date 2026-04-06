4b1e797a80d682f1ca3fbc5db2a47d00f2a6f3c7
“96Nerr” <naufalriper03@gmail.com>
2026-04-07 03:34:43 +0700

    tes

--- Belajar/1/Person.java
+++ Belajar/1/Person.java
@@ -1,25 +1,37 @@
 class Person{
     String nama;
     int umur;
+    String KataSandi;
     final String country = "Indonesia";
 
     //constructor
-    Person(String ParamNama, int ParamUmur){
+    Person(String ParamNama, int ParamUmur, String pin){
         nama = ParamNama;
         umur = ParamUmur;
+        KataSandi = pin;
     }
-
+    
     //constructor overloading (param harus beda (tipe data atau jumlahnya))
     Person(String ParamNama){
-        nama = ParamNama;
+        this(ParamNama, 0, "XXXXX");
     }
 
     //constructor kosong
     Person(){
+        this("Unknown", 0, "XXXXX");
+    }
+
+    public void DataDiri(){
+        
     }
 
     void sapa(){
         System.out.println ("halo nama saya " + nama + ", umur saya " + umur + " Tahun");
     }
 
+    void Login(String Nickname, String password){
+
+        if (Nickname.equals(nama) && password.equals(KataSandi)){
+        System.out.println ("Selamat Datang Kembali " + nama);   }
+    }
 }
